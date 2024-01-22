bea991d92e66bfcaa4c204c62ed047ec1844a2f6
Mrzakirullahkhan <mrzak802@gmail.com>
2024-01-22 16:14:01 +0500

    comment

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import "./CSS/LoginSignUp.css"
 
 function LoginSignup() {
   return (
-    <div>LoginSignup</div>
+    <div className='loginSignUp'>
+      <div className="loginSignup-conatiner">
+        <h1>Sign Up</h1>
+        <div className="loginSignup-fields">
+          <input type="text" placeholder='Enter your name' />
+          <input type="email" placeholder='Enter your email' />
+          <input type="password" placeholder='password' />
+        </div>
+        <button>Sign Up</button>
+        <p className="loginsignup-login">
+          Already have an account ? <span>Login</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox"  id='' name=''/>
+          <p>i agree to the term of use & privacy policy</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/ShopCatogary.jsx
+++ frontend/src/Pages/ShopCatogary.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react'
 import "./CSS/ShopCatogary.css"
 import { shopContext } from '../Context/ShopContext'
+import Item from "../Components/item/Item"
 import dropdown_icon from "../Components/Assets/dropdown_icon.png"
 function ShopCatogary(props) {
   const {all_products} = useContext(shopContext);
+
   return (
     <div className='shop-catogary'>
-      <img src={props.banner} alt="" />
+      <img className='shopcatogary-banner' src={props.banner} alt="" />
       <div className="shopcatogary-indexSort">
         <p>
           <span>showing 1-12</span> out of 36 products
@@ -14,6 +16,19 @@ function ShopCatogary(props) {
         <div className='shopcatogary-sort'>
           sort by <img src={dropdown_icon} alt="" />
         </div>
+      </div>
+      <div className="shopcatogary-product">
+        {all_products.map((item,i)=>{
+          if(props.Catogary===item.category){
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+          }
+          else{
+            return null
+          }
+        })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore more
       </div>
       </div>
   )
