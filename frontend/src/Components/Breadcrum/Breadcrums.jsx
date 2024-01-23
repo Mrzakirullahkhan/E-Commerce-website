c6fc346eeb194cfd077cabf7f035e4ae85bdfb5c
Mrzakirullahkhan <mrzak802@gmail.com>
2024-01-24 00:42:11 +0500

    comment

--- frontend/src/Components/Breadcrum/Breadcrums.jsx
+++ frontend/src/Components/Breadcrum/Breadcrums.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./Breadcrums"
+import "./Breadcrum.css"
 import arrow_icon from "../Assets/breadcrum_arrow.png"
 function Breadcrums(props) {
     const {product} = props
--- frontend/src/Components/productDisplay/ProductDisplay.jsx
+++ frontend/src/Components/productDisplay/ProductDisplay.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./ProductDisplay"
+import "./ProductDisplay.css"
 import start_icon from "../Assets/star_icon.png";
 import start_dull_icon from "../Assets/star_dull_icon.png";
 
@@ -12,7 +12,7 @@ function ProductDisplay(props) {
             <img src={product.image} alt="" />
             <img src={product.image} alt="" />
             <img src={product.image} alt="" />
-            <img src={product.image} alt="" />
+            
         </div>
         <div className="productdisplay-image">
             <img className='product-main-img' src={product.image} alt="" />
@@ -28,6 +28,30 @@ function ProductDisplay(props) {
                 <img src={start_dull_icon} alt="" />
                 <p>(112)</p>
             </div>
+            <div className="product-dispplay-right-price">
+                <div className="product-display-right-price-old">
+                    {product.old_price}
+                </div>
+                <div className="product-display-right-price-new">
+                    {product.new_price}
+                    </div>
+            </div>
+            <div className="product-display-right-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint incidunt quo mollitia nihil at sapiente. Voluptate cupiditate cumque aperiam obcaecati pariatur odio voluptatibus architecto! Nam nesciunt iure ab sed numquam?
+            </div>
+            <div className="product-display-right-size">
+                <h1>Select Size</h1>
+                <div className="product-display-right-sizes">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button>ADD TO CARD</button>
+            <p className='product-display-right-catogary'><span>Catogary: </span>Women, T-shart, Crop Top</p>
+            <p className='product-display-right-catogary'><span>Tags: </span>modern latest </p>
         </div>
      </div>
   )
